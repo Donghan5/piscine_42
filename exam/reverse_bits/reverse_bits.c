645dc70c4ab79c2e2e70fa105b615e14aeb6fca4
donghank <donghan.kim02@gmail.com>
2024-04-03 16:09:51 +0200

    I have to study more

--- exam/reverse_bits/reverse_bits.c
+++ exam/reverse_bits/reverse_bits.c
@@ -1,5 +1,13 @@
-#include <unistd.h>
-
 unsigned char	reverse_bits(unsigned char octet)
 {
-	
+	unsigned char	res;
+	int	i;
+
+	i = 8;
+	while (i--)
+	{
+		res = res * 2 + (octet % 2);
+		octet /= 2;
+	}
+	return (res);
+}
