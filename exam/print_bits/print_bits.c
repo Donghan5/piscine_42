9ad0f18a5f97cec12873e942a25f83de38fe9aa7
donghank <donghan.kim02@gmail.com>
2024-04-04 13:35:30 +0200

    dds

--- exam/print_bits/print_bits.c
+++ exam/print_bits/print_bits.c
@@ -6,30 +6,9 @@ void	print_bits(unsigned char octet)
 	int	i;
 
 	i = 8;
-	binary = 0;
-	while (i > 0)
+	while (i--)
 	{
-		binary = binary * 2 + (octet % 2);
-		octet /= 2;
+		binary = (octet >> i & 1) + '0';
 		write(1, &binary, 1);
-		i--;
 	}
 }
-
-int main(void)
-{
-	print_bits(0);
-	write(1, "\n", 1);
-	print_bits(1);
-	write(1, "\n", 1);
-	print_bits(2);
-	write(1, "\n", 1);
-	print_bits(10);
-	write(1, "\n", 1);
-	print_bits(113);
-	write(1, "\n", 1);
-	print_bits(255);
-	write(1, "\n", 1);
-	return (0);
-}
-
